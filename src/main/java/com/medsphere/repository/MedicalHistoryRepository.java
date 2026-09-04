cd6c1b612a32f0fa836e516b21a6021533c7f3ec
Krushna Manthalkar <krishnamanthalkar@gmail.com>
2026-09-04 14:33:21 +0530

    Add medical history patient lookup

--- src/main/java/com/medsphere/repository/MedicalHistoryRepository.java
+++ src/main/java/com/medsphere/repository/MedicalHistoryRepository.java
@@ -3,5 +3,9 @@ package com.medsphere.repository;
 import com.medsphere.entity.MedicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
+
+    List<MedicalHistory> findByPatientIdOrderByRecordDateDesc(Long patientId);
 }
